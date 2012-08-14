f1d8f9e35ac9dcfdd12dfc0d70ae6b98d6d761ab
mid0111 <m.tajima0111@gmail.com>
2012-08-14 15:31:22 +0900

    create Calculator class and describe test code.

---
+++ src/main/java/sample/Calculator.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Calculator {
+
+	public Calculator(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int add() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
--- src/test/java/sample/DoAddScenarios.java
+++ src/test/java/sample/DoAddScenarios.java
@@ -19,9 +19,12 @@ public class DoAddScenarios {
 	@Scenario
 	public void shouldGivePositiveNumbers() {
 		int result = 0;
+
 		Given("20 and 30");
+		Calculator calc = new Calculator(20, 30);
 
 		When("addボタンを押す");
+		result = calc.add();
 
 		Then("50を表示");
 		assertThat(result, is(50));
@@ -31,8 +34,10 @@ public class DoAddScenarios {
 	public void shouldGiveNegativeNumbers() {
 		int result = 0;
 		Given("-20 and -30");
+		Calculator calc = new Calculator(20, 30);
 
 		When("addボタンを押す");
+		result = calc.add();
 
 		Then("-50を表示");
 		assertThat(result, is(-50));
