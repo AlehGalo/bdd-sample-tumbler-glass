03f32d2dfb49c776e3734f28cfe5950ca380a24e
mid0111 <m.tajima0111@gmail.com>
2012-08-14 15:25:18 +0900

    add assert statements.

--- src/test/java/sample/DoAddScenarios.java
+++ src/test/java/sample/DoAddScenarios.java
@@ -1,30 +1,40 @@
 package sample;
-import org.junit.*;
-import org.junit.runner.*;
-import static org.junit.Assert.*;
-import junitparams.*;
-import tumbler.*;
-import static tumbler.Tumbler.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static tumbler.Tumbler.Given;
+import static tumbler.Tumbler.Then;
+import static tumbler.Tumbler.When;
+
+import org.junit.runner.RunWith;
+
+import tumbler.Scenario;
+import tumbler.Story;
+import tumbler.TumblerRunner;
 
 @RunWith(TumblerRunner.class)
 @Story("do add")
 public class DoAddScenarios {
 
-	@Scenario(pending = true)
+	@Scenario
 	public void shouldGivePositiveNumbers() {
+		int result = 0;
 		Given("20 and 30");
 
 		When("addボタンを押す");
 
 		Then("50を表示");
+		assertThat(result, is(50));
 	}
 
-	@Scenario(pending = true)
+	@Scenario
 	public void shouldGiveNegativeNumbers() {
+		int result = 0;
 		Given("-20 and -30");
 
 		When("addボタンを押す");
 
 		Then("-50を表示");
+		assertThat(result, is(-50));
 	}
 }
